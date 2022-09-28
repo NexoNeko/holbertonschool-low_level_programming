d6b584d59b35357ac6d90a85c3f3407a279ae8d8
NexoNeko <meresidamente@hotmail.com>
2022-09-27 18:55:55 -0700

    Autocommit

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the max. val. in bytes for each data type.
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a char: %ld byte(s)\n", sizeof(int));
+	printf("Size of a char: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a char: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a char: %ld byte(s)\n", sizeof(float));
+	return (0);
+}

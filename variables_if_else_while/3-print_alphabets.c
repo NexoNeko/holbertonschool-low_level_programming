eda9eb108c7ffa7a76b09225815ecd15de1c97b3
NexoNeko <meresidamente@hotmail.com>
2022-09-28 09:45:04 -0700

    Autocommit

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a < '{')
+	{
+		putchar(a);
+		a++;
+	}
+	a = 'A';
+	while (a < '[')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}

2380048465cf4283157c1997a2b2ef4739fdb8d3
NexoNeko <meresidamente@hotmail.com>
2022-09-30 06:21:35 -0700

    Autocommit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Prints '_putchar'
  *
  * Return: 0
  */
-int main()
+int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char a[10] = "_putchar\n";
 
+	for (i = 0; i < 9; i++)
+		_putchar(a[i]);
 	return (0);
 }

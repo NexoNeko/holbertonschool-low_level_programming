6774218006f947eece6dee90f78cb2b277f5d8f6
NexoNeko <meresidamente@hotmail.com>
2022-09-30 08:38:30 -0700

    Autocommit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,8 @@
 /**
  * print_alphabet - Prints the alphabet
  *
- * _putchar - prints a character to stdout
- *
- *@c: the character to be printed
- *
  * Return: Character
  */
-
-int _putchar(char c);
-
 void print_alphabet(void)
 {
 	char a = 'a';

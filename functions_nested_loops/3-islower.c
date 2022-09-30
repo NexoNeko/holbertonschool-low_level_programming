fc0428d0b9921a275f064e24525128d9b1b20ceb
NexoNeko <meresidamente@hotmail.com>
2022-09-30 08:57:20 -0700

    Autocommit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+/**
+ *_islower - checks wether the char is lowercase or not
+ *
+ *
+ *@c: The char to be tested
+ *
+ * Return: 0 or 1
+ */
+int_islower(int c);
+{
+	if (c == 99)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

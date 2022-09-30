105306f118dfa931c7401e6adc52c582c3917152
NexoNeko <meresidamente@hotmail.com>
2022-09-30 09:45:06 -0700

    Autocommit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - returns last digit of an integer
+ *
+ *
+ * @i: The num to be cut
+ *
+ * Return: i
+ */
+int print_last_digit(int i)
+{
+	i = i % 10;
+	return (i);
+}

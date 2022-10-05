061361d45d15d582a252d4194df32a2a2b4a5a46
NexoNeko <meresidamente@hotmail.com>
2022-10-04 19:58:54 -0700

    Autocommit

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+/**
+ * print_triangle - prints a triangle of size size
+ *
+ * @c: Char to be printed
+ *
+ * Return: void
+ */
+int _putchar (char c);
+void print_triangle(int size)
+{
+	int a, s;
+
+	s = size;
+	a = 0;
+	while (s != 0)
+	{
+		while (a < s)
+		{
+			if ((s - 1) == a)
+				;
+			else
+				_putchar(' ');
+			a++;
+		}
+		while (a >= s && a <= size)
+		{
+			_putchar('#');
+			a++;
+		}
+		_putchar('\n');
+		a = 0;
+		s--;
+		}
+	if (size <= 0)
+		_putchar('\n');
+}

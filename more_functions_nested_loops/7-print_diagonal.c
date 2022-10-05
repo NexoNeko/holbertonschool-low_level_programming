60a1768bc6fb47096b639131c5c6783acae18823
NexoNeko <meresidamente@hotmail.com>
2022-10-04 19:08:35 -0700

    Autocommit

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+/**
+ * print_diagonal - prints a diagonal of n size
+ *
+ * @c: Char to be printed
+ *
+ * Return: void
+ */
+int _putchar (char c);
+void print_diagonal(int n)
+{
+	int a = 0,s = 0;
+
+	while (a < n && n > 0)
+	{
+		while (s != a)
+		{
+			_putchar(' ');
+			s--;
+		}
+		_putchar('\\');
+		a++;
+		s = (a * 2);
+		if (a == n)
+			;
+		else
+		_putchar('\n');
+	}
+	_putchar('\n');
+}

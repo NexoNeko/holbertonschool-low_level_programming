771b8fb4c6e8769dc91844222a41bc61b205001f
NexoNeko <meresidamente@hotmail.com>
2022-10-04 19:26:24 -0700

    Autocommit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+/**
+ * print_square - prints a square of size size.
+ *
+ * @c: Char to be printed
+ *
+ * Return: void
+ */
+int _putchar (char c);
+void print_square(int size)
+{
+	int a = 0, s;
+
+	s = size;
+	while (s != 0)
+	{
+	while (a < size && size > 0)
+	{
+		_putchar('#');
+		a++;
+	}
+	_putchar('\n');
+	s--;
+	a = 0;
+	}
+	if (size <= 0)
+	_putchar('\n');
+}

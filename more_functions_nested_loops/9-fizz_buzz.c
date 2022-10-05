5b3b7ab726ba01ebd5941800f5ccc588f5ef1b57
NexoNeko <meresidamente@hotmail.com>
2022-10-04 20:34:42 -0700

    Autocommit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+/**
+ * fizz_buzz - prints 1..100 replacing %3=0, %5=0 && %3&%5=0 with strings
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+	int a = 1;
+
+	while (a <= 100)
+	{
+		if ((a % 3) == 0 && (a % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((a % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", a);
+		if (a != 100)
+			putchar(' ');
+		a++;
+	}
+	putchar('\n');
+}

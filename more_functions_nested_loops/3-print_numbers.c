e079d021109b2726021c423f910e67ad6f8ba304
NexoNeko <meresidamente@hotmail.com>
2022-10-04 18:19:36 -0700

    Autocommit

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
 /**
  * print_numbers - prints from 0 to 9\n.
  *
+ * @c: Char to be printed
+ *
  * Return: void
  */
+int _putchar (char c);
 void print_numbers(void)
 {
 	int a = 0;
 
-	while (a != 9)
+	while (a <= 9)
 	{
-		putchar(a + 48);
+		_putchar(a + 48);
 		a++;
 	}
-	printf("%d\n", a);
+	_putchar('\n');
 }

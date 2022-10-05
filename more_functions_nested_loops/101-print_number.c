cdbcbdfa7f6c0d888ddf1128a357092e96ca3810
NexoNeko <meresidamente@hotmail.com>
2022-10-05 16:40:11 -0700

    Autocommit

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+/**
+ * print_number - prints int n
+ *
+ * @c: Char to be printed
+ *
+ * Return: void
+ */
+int _putchar (char c);
+void print_number(int n)
+{
+	int i;
+
+	i = 0;
+	if (n < 0)
+	{
+		n /= -1;
+		_putchar('-');
+	}
+	if (n > 999)
+	{
+		i = (((n / 10) / 10) / 10);
+		_putchar(i + 48);
+		i *= 1000;
+		_putchar((((n - i) / 10) / 10) + 48);
+		i = (i / 1000) * 100;
+		_putchar((((n - i) / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n > 99)
+	{
+		i = (((n / 10) / 10));
+		_putchar(i + 48);
+		i *= 100;
+		_putchar(((n - i) / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n > 9)
+	{
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else
+		_putchar(n + 48);
+	return (0);
+}

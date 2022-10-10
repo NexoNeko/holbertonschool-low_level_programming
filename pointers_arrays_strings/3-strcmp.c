fd767112f4fc9d3937c58f9778cf7fdf27300ea8
NexoNeko <meresidamente@hotmail.com>
2022-10-10 16:03:13 -0700

    Autocommit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, c, r;
+	int i, c, r, l;
 
 	i = r = c = 0;
-	while (i < 10)
+	while (s1[c++])
+		;
+	while (s2[r++])
+		;
+	if (c < r)
+		l = c;
+	else
+		l = r;
+	i = r = c = 0;
+	while (i < l)
 	{
 		if (s1[i] < s2[i])
 			c++;

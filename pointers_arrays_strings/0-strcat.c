0e65c87e50d2e006e6f866ba930c3bf7ca6dbf23
NexoNeko <meresidamente@hotmail.com>
2022-10-10 08:32:48 -0700

    Autocommit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: String to be expanded
+ * @src: text to be pasted into previous string
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+	char *a;
+
+	c = -1;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	while(src[++c])
+	{
+		dest[i] = src[c];
+		i++;
+	}
+	a = dest;
+	return (a);
+}

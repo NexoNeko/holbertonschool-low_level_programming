b21514bac6548f134c3eb586f21e2f94b84e680e
NexoNeko <meresidamente@hotmail.com>
2022-10-11 17:19:16 -0700

    Autocommit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+/**
+ * puts2 - prints every other character, starting from the first
+ *
+ * @c: char printed
+ *
+ * Return: void
+ */
+char *cap_string(char *p)
+{
+	char c[14] = {9, 10, 32, 33, 34, 40, 41, 44, 59, 46, 63, 123, 125};
+	int i, n;
+
+	i = n = 0;
+	for (n = 0; p[n] != '\0'; n++)
+	{
+		printf (" %c ", p[n]);
+		i = 0;
+		for(; c[i] != '\0'; )
+		{
+			if (p[n] == c[i])
+			{
+				n++;
+				if (p[n] < 96)
+				{
+					n++;
+					if (p[n] > 96 && p[n] < 123)
+						p[n] -= 32;
+				}
+				else if (p[n] > 96 < 123)
+					p[n] -= 32;
+			}
+			i++;
+		}
+	}
+	return (p);
+}

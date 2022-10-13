62db80d70d28d2848449de03524b728740bf4aa3
NexoNeko <meresidamente@hotmail.com>
2022-10-12 18:36:20 -0700

    Autocommit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+/**
+ * _strstr - finds a specific char sequence within a string
+ *
+ * @haystack: string containing desired character sequence
+ * @needle: desired character sequence to look for
+ *
+ * Return: haystack + i || needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, n = 1;
+
+	while (haystack[i])
+	{
+		while (needle[n] > 0 && haystack[i] == needle[0])
+		{
+			if (haystack[i + n] == needle[n])
+				n++;
+				else
+					break;
+		}
+		if (needle[n])
+		{
+			i++;
+			n = 1;
+		}
+		else
+			return (haystack + i);
+	}
+	return (haystack);
+}

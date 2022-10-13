f0dbe3b44287596ce954e8e18be456a34faa9f66
NexoNeko <meresidamente@hotmail.com>
2022-10-13 08:58:05 -0700

    Autocommit

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_diagsums - sums diagonal values on an array and prints the result
+ *
+ * @a: Array containing the values
+ * @size: Size of the array
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, reslt, reslt2, R;
+
+	i = reslt = R = 0;
+	R = size;
+	while (i < (R - 1))
+	{
+		reslt += a[size + i + 1];
+		size += R;
+		i++;
+	}
+	i = reslt2 = 0;
+	size = R;
+	while (i < R)
+	{
+		reslt2 += a[size - i - 1];
+		size += R;
+		i++;
+	}
+	printf("%d, %d\n", reslt, reslt2);
+}
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,20 +1,26 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
-char *_strstr(char *, char *);
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *s = "First, solve the problem. Then, write the code.";
-	char *f = "";
-	char *t;
-
-	t = _strstr(s, f);
-	printf("%s\n", (t == NULL ? "nil" : t));
+	int c3[3][3] = {
+		{0, 1, 5},
+		{10, 11, 12},
+		{1000, 101, 102},
+	};
+	int c5[5][5] = {
+		{0, 1, 5, 12124, 1234},
+		{10, 11, 12, 123521, 12512},
+		{1000, 101, 102, 12545, 214543435},
+		{100, 1012451, 11102, 12545, 214543435},
+		{10, 12401, 10452, 11542545, 1214543435},
+	};
+	print_diagsums((int *)c3, 3);
+	print_diagsums((int *)c5, 5);
 	return (0);
 }

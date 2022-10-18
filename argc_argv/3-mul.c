70e6b0138de62c7be9496fc1dd15aa728ef5f0b0
NexoNeko <meresidamente@hotmail.com>
2022-10-18 06:11:14 -0700

    Autocommit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments passed to the program
+ *
+ * @argc: num of arguments (int)
+ * @argv: arguments (array of *)
+ *
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	char *p;
+	long a = strtol(argv[1], &p, 10);
+	long b = strtol(argv[2], &p, 10);
+
+	if (argc > 1)
+		printf("%ld\n", a * b);
+	else
+		printf("Error");
+	return (0);
+}

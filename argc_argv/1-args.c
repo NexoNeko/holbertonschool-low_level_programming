d8784cbf2d445b5b137bfcd92b2fcc00bbd085cc
NexoNeko <meresidamente@hotmail.com>
2022-10-18 05:19:56 -0700

    Autocommit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - returns the program name
+ *
+ * @argc: num of arguments (int)
+ * @argv: arguments (array of *)
+ *
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	printf ("%d\n", argc);
+	argv + 1;
+	return (0);
+}

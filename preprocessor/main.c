2d824542a1f26de7b8a7734e8a4f3ee7a5504c47
NexoNeko <meresidamente@hotmail.com>
2022-10-24 11:46:55 -0700

    Autocommit

--- preprocessor/main.c
+++ preprocessor/main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include "3-function_like_macro.h"
+#include "4-sum.h"
+#include "4-sum.h"
 
 /**
- * main - check the code .
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-#ifndef ABS
-	printf("The macro 'ABS' is not defined\n");
-	return (1);
-#else
-	printf("%d\n", ABS(1024 - 2048));
-		printf("%d\n", ABS(1024));
-			printf("%d\n", ABS(-1024));
-	return (0);
-#endif
+    int s;
+
+    s = SUM((15 + 98), 1024);
+    printf("%d\n", s);
+    return (0);
 }

865d92663d591cfa0a88813c6dbe4796efc17859
NexoNeko <meresidamente@hotmail.com>
2022-10-24 09:07:54 -0700

    Autocommit

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,8 +13,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	c = max - min;
-	ar = malloc(c * sizeof(int) + 1);
+	ar = malloc(max * sizeof(int) + 1);
 	if (!ar)
 		return(NULL);
 

21842c43cbec40bb610a5ee7c6eb221ec2f6f911
NexoNeko <meresidamente@hotmail.com>
2022-10-24 05:10:27 -0700

    Autocommit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - assigns the desired memory using malloc
+ *
+ * @b: ammount of memory to be assigned
+ *
+ * Return: pointer to memory, exits w/98 if failed to alloc
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *str = NULL;
+
+	str = malloc(b * sizeof(int));
+	if (!str)
+		exit(98);
+	else
+		return (str);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this area there will be more exercises about malloc and free

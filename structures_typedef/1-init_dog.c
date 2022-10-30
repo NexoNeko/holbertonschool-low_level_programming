2d83c6efffdf5e13bf2a12f589078112943e110a
NexoNeko <meresidamente@hotmail.com>
2022-10-30 08:51:19 -0700

    Autocommit

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a var of type dog
+ *
+ * @d: pointer to dog variable
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: whom owns the dog
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,4 +13,8 @@ struct dog
 	char *owner;
 	float age;
 };
+/**
+ * init_dog - initializes a var of type dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

c048b5916e5035a03e3f7452d0638937d2be0c5f
NexoNeko <meresidamente@hotmail.com>
2022-10-30 15:05:55 -0700

    Autocommit

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,12 +18,18 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 	sgn = argv[2];
 
+	if ((*sgn == '/' || *sgn == '%') && (b == 0))
+	{
+		printf ("Error\n");
+		exit (100);
+	}
+
 	op = get_op_func(sgn);
 
 	if (op == NULL)
 	{
 		printf("Error\n");
-		return (99);
+		exit (99);
 	}
 
 	printf("%d\n", op(a, b));

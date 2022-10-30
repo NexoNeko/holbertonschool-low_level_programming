dce58bb47c4d5dbe767a4215f033e837d1b07523
NexoNeko <meresidamente@hotmail.com>
2022-10-30 11:38:52 -0700

    Autocommit

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,13 +4,17 @@
  *
  * @array: pointer to an array
  * @size: size of the array
+ * @action: pointer to a function
  *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	do {
-		action(array[i]);
-	} while (++i < size);
+	if (size > 0 && action != NULL && array != NULL)
+	{
+		do {
+			action(array[i]);
+		} while (++i < size);
+	}
 }

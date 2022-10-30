38d2a4cc8c383a6b41e5849c012b3ccc185e5e9a
NexoNeko <meresidamente@hotmail.com>
2022-10-30 18:32:40 -0300

    Exercise files
    I don't feel like writing them on emacs man, I just wrote them on notepad k?thxby

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -1,55 +1,24 @@
 #include <stdio.h>
-#include "function_pointers.h"
+#include <stdlib.h>
 
-/**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_98(int elem)
-{
-    return (98 == elem);
-}
+int main(int argc, char *argv[])
+	int i;
+	int a, b;
+	char *sgn;
+	int (*op)(int, int);
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-    return (elem > 0);
-}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	sgn = argv[2];
 
+	op = get_op_func(sgn);
+	if (op == NULL)
+	{
+		printf("Error");
+		return (-1);
+	}
 
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-    return (elem == 98 || -elem == 98);
-}
+	printf("%d\n", op(a, b));
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
-
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
+	return (0);
 }

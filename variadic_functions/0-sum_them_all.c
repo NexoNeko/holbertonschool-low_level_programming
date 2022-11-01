7b87c05490fea4cdd255ed5e87de86f24e7ac68c
NexoNeko <meresidamente@hotmail.com>
2022-11-01 08:16:40 -0700

    Autocommit

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
  *
  * Return: int sum
  */
- int sum_them_all(const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum;
 	va_list holds;
@@ -12,12 +12,12 @@
 	if (n == 0)
 		return (0);
 
-	va_start (holds, n);
+	va_start(holds, n);
 	sum = 0;
 
 	for (i = 0; i < n; i++)
-		sum += va_arg (holds, int);
+		sum += va_arg(holds, int);
 
 	va_end(holds);
-	return(sum);
+	return (sum);
 }

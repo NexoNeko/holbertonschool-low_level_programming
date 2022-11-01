73c6df9318fd73ce907aab670780e4bc9ae8ba89
NexoNeko <meresidamente@hotmail.com>
2022-11-01 08:42:17 -0700

    Autocommit

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints numbers using a separator
+ *
+ * @separator: The separator character
+ * @n: number of integers passed to the function
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list word;
+
+	va_start(word, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(word, char*));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(word);
+
+	printf("\n");
+}

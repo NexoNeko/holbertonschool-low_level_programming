cd777dc0dd4385ca6320a9e01442937afb07d476
NexoNeko <meresidamente@hotmail.com>
2022-11-01 12:11:01 -0300

    exercise

---
+++ variadic_functions/strdt.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all the parameters sent to the function
+ *
+ * Return: int sum
+ */
+ int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum;
+	va_list holds;
+
+	if (n == 0)
+		return (0);
+
+	va_start (holds, n);
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+		sum += va_arg (holds, int);
+
+	va_end(holds);
+	return(sum);
+}

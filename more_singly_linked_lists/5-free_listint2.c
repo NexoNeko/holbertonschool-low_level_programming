164f2aa9dcc6e8d77f65f9dfe42721bb9d8ca4b5
NexoNeko <meresidamente@hotmail.com>
2022-11-16 10:27:35 -0800

    Autocommit

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ *
+ * @head: list to free;
+ *
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	if (head)
+	{
+		listint_t *temp = *head;
+		listint_t *next = NULL;
+
+		while (temp || next)
+		{
+			next = temp->next;
+			free(temp);
+			temp = next;
+		}
+		*head = NULL;
+	}
+}

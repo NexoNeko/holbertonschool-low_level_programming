3daaac9daf4c17413643a7485ba1f51c634fc00e
NexoNeko <meresidamente@hotmail.com>
2022-11-16 05:09:16 -0800

    Autocommit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * print_list - Prints all the values inside a linked list
+ *
+ * @h: Head of list to print
+ *
+ * Return: num of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int i = 0;

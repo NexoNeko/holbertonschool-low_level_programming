b9babe86b9dee48fc73a51fe3b6e68a759cb4276
NexoNeko <meresidamente@hotmail.com>
2022-11-16 09:29:27 -0800

    Autocommit

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint - prints the values in a list of ints
+ *
+ * @h: list to print the values of
+ *
+ * Return: Num of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->n)
+			printf("%d\n", h->n);
+		else
+			printf("0\n");
+		i++;
+		h = h->next;
+	}
+	return (i);
+}

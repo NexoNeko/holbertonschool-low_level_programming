05c6846ddbd76303e1d205484406c0403d85d2dd
NexoNeko <meresidamente@hotmail.com>
2022-11-16 09:52:18 -0800

    Autocommit

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -13,9 +13,9 @@ size_t print_listint(const listint_t *h)
 	while (h)
 	{
 		if (h->n)
-			printf("%d\n", h->n);
+			printf("%d", h->n);
 		else
-			printf("0\n");
+			printf("0");
 		i++;
 		h = h->next;
 	}

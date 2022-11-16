565f8623414748a5adf15d4b20e981458ce786fd
NexoNeko <meresidamente@hotmail.com>
2022-11-16 06:20:06 -0800

    Autocommit

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node - pushes a new node into the list
+ *
+ * @head: the list to push the node into
+ * @str: string to be contained by the new node
+ *
+ * Return: Void
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nuNode = (list_t *) malloc(sizeof(list_t));
+
+	if (!nuNode || !head)
+		free(nuNode);
+	else
+	{
+		nuNode->str = strdup(str);
+		nuNode->next = (*head);
+		nuNode->len = strlen(str);
+		(*head) = nuNode;
+	}
+}

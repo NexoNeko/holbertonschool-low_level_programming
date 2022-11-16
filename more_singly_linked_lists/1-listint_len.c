6290870c9fe7c2eec08ea191b8cd1aa320eae010
NexoNeko <meresidamente@hotmail.com>
2022-11-16 09:47:52 -0800

    Autocommit

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -6,7 +6,7 @@
  *
  *Return: Num of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int i = 0;
 
--- more_singly_linked_lists/main.c
+++ more_singly_linked_lists/main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -22,7 +22,7 @@ int main(void)
     new->n = 9;
     new->next = head;
     head = new;
-    n = print_listint(head);
+    n = listint_len(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (0);

5a3e92465012ae53d0cf2304941505e3df6d01e2
NexoNeko <meresidamente@hotmail.com>
2022-11-19 15:38:08 -0800

    Autocommit

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,36 +1,43 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - deltes the node at the given index
+ * delete_nodeint_at_index - deletes the node at the specific index
  *
- * @head: The list to go through
- * @index: Index of the node to delete
+ * @head: List to go through
+ * @index: Nth of index in which to add the new node
  *
- * Return: 1 on success, -1 on failure.
+ * Return: Address of new node, -1 it fails.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *olNode = NULL, *nexNod = NULL;
+	listint_t *temp, *nexNodd;
+	unsigned int i = 0;
 
-	if (!head || !*head)
+	if (head == NULL || *head == NULL)
 		return (-1);
-	olNode = *head;
-	nexNod = (*head)->next;
 
+	temp = *head;
 	if (index == 0)
-		(*head) = nexNod;
-	else
 	{
-		while (index > 0)
-		{
-			olNode = nexNod;
-			nexNod = nexNod->next;
-			index--;
-			if (!olNode)
-				return (-1);
-			if (index == 1)
-				olNode->next = nexNod->next;
-		}
+		*head = temp->next;
+		free(temp);
+		return (1);
 	}
-	free(olNode);
+	if (index == 1)
+	{
+		temp = (*head)->next;
+		(*head)->next = temp->next;
+		free(temp);
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		if (temp->next == NULL)
+			return (-1);
+		i++;
+		temp = temp->next;
+		nexNodd = temp->next;
+	}
+	temp->next = nexNodd->next;
+	free(nexNodd);
 	return (1);
 }

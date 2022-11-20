8066743e7cb1e46e2a3b84fc82dd3e13d97d8455
NexoNeko <meresidamente@hotmail.com>
2022-11-20 10:42:49 -0800

    Autocommit

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * clear_bit - sets the bit to 0 at given index
+ *
+ * @n: decimal to convert
+ * @index: index of bit to clear
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int temp, c = 0;
+
+	if (n == NULL)
+		return (-1);
+
+	temp = *n;
+	while (temp != 0)
+	{
+		temp = temp >> 1;
+		c++;
+	}
+	if (index > 64)
+		return (-1);
+
+	temp = 1 << index;
+	if ((temp | *n) == *n)
+		*n = *n ^ temp;
+	return (1);
+}

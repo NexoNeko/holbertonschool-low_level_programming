c1f9ecfdf9da692bdb1e881984ba4aa96fcbe11b
NexoNeko <meresidamente@hotmail.com>
2022-11-20 09:03:29 -0800

    Autocommit

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary equivalent of a decimal
+ *
+ * @n: decimal to convert
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int c = 0, k = 0, temp = n;
+
+	if (n == 0)
+		putchar('0');
+	while (temp != 0)
+	{
+		temp = temp >> 1;
+		c++;
+	}
+	for (c--; c >= 0; c--)
+	{
+		k = n >> c;
+		if (k & 1)
+			putchar('1');
+		else
+			putchar('0');
+	}
+}

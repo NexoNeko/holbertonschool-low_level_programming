89721a07058afe7a875fc41ee866074a8849be83
NexoNeko <meresidamente@hotmail.com>
2022-11-20 09:56:16 -0800

    Autocommit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,31 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int k = 0;
-	unsigned long int temp = n, c = 0;
+	unsigned int ret;
 
-	if (n == 0)
-		putchar('0');
-	while (temp != 0)
-	{
-		temp = temp >> 1;
-		c++;
-	}
-	c--;
-	if (index <= c)
-	{
-		for (; ; c--)
-		{
-			k = n >> c;
-			if (c == index)
-			{
-				if (k & 1)
-					return (1);
-				else
-					return (0);
-			}
-		}
-	}
+	ret = (n >> index) & 1;
+
+	if ((ret == 1) || (ret == 0))
+		return (ret);
 	else
 		return (-1);
 }

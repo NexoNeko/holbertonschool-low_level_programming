4e1a1196adaa11f742939319d9c64424c81492be
NexoNeko <meresidamente@hotmail.com>
2022-11-23 05:41:08 -0800

    Autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,6 +4,8 @@
  *
  * @filename: the file name to create
  * @text_content: content of the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -12,8 +14,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename)
 	{
 		newzFlz = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (newzFlz == -1)
+		if (newzFlz == -1 && access(filename, F_OK) == -1)
 			return (-1);
+		else
+			newzFlz = creat(filename, 0600);
 
 		if (text_content != NULL)
 		{

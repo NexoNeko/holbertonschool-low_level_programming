77174b40054b10e7c85c5c8b4eef71ff9a964144
NexoNeko <meresidamente@hotmail.com>
2022-11-23 06:24:44 -0800

    Autocommit

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - creates a file with rw permissions
+ *
+ * @filename: the file name to create
+ * @text_content: content of the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int newzFlz, i, writz;
+
+	if (filename)
+	{
+		newzFlz = open(filename, O_WRONLY | O_APPEND | S_IRUSR | S_IWUSR);
+
+		if (access(filename, F_OK) == -1)
+			return (-1);
+		if (newzFlz == -1)
+			return (-1);
+
+		if (text_content != NULL)
+		{
+			for (i = 0; text_content[i]; i++)
+				;
+			writz = write(newzFlz, text_content, i);
+			if (writz == -1)
+				return (-1);
+		}
+		if (access(filename, F_OK) != -1)
+			return (1);
+	}
+	return (-1);
+}

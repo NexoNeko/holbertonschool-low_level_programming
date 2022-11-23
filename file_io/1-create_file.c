2e74394e42731e0b237b01e727a54be94c1c6b8c
NexoNeko <meresidamente@hotmail.com>
2022-11-23 05:02:09 -0800

    Autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,10 +16,9 @@ int create_file(const char *filename, char *text_content)
 		if (access(filename, R_OK) != 0 && access(filename, F_OK) == 0)
 			return (-1);
 
+		filz = creat(filename, 0600);
 		if (filz == -1)
 			return (-1);
-
-		filz = creat(filename, 0600);
 
 		if (text_content)
 		{

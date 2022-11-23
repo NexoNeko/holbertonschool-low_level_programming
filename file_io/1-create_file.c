dc5707cf2b2616adaa63e9376b0cd7524eb67cab
NexoNeko <meresidamente@hotmail.com>
2022-11-23 05:47:58 -0800

    Autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,11 +25,9 @@ int create_file(const char *filename, char *text_content)
 			for (i = 0; text_content[i]; i++)
 				;
 			writz = write(newzFlz, text_content, i);
-			if (writz != -1)
-				return (1);
+			if (writz == -1)
+				return (-1);
 		}
-		else if (newzFlz != -1)
-			return (1);
 	}
-	return (-1);
+	return (1);
 }

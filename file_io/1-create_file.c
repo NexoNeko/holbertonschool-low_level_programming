27c301195e0cd30368e5d15c432299f66a6b8db0
NexoNeko <meresidamente@hotmail.com>
2022-11-23 05:37:47 -0800

    Autocommit

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - creates a file with rw permissions
+ *
+ * @filename: the file name to create
+ * @text_content: content of the file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int newzFlz, i, writz;
+
+	if (filename)
+	{
+		newzFlz = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (newzFlz == -1)
+			return (-1);
+
+		if (text_content != NULL)
+		{
+			for (i = 0; text_content[i]; i++)
+				;
+			writz = write(newzFlz, text_content, i);
+			if (writz != -1)
+				return (1);
+		}
+	}
+	return (-1);
+}

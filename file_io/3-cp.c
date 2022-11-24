738ec1d01d2893a84e8495dd727eeaf185e15ef2
NexoNeko <meresidamente@hotmail.com>
2022-11-24 07:16:02 -0800

    autocommit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,21 +13,21 @@ int main(int argc, char **argv)
 		errz(97, NULL);
 	else
 		create_file(argv[1], argv[2]);
-	return (1);
+	return (0);
 }
 
 
 /**
  * create_file - creates a file with rw permissions
  *
- * @filename: the file name to create
- * @text_content: content of the file
+ * @toFi: the file name to create
+ * @fromFi: the file to read from
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *fromFi, char *toFi)
 {
-	int newzFlz, i, j, writz;
+	int newzFlz, writz, i, j;
 	char *bufz = NULL;
 	FILE *oleFlz = fopen(fromFi, "r");
 
@@ -46,6 +46,7 @@ int create_file(const char *fromFi, char *toFi)
 	bufz = malloc(sizeof(char) * 1024);
 	if (!bufz)
 		errz(100, NULL); /** error 100 */
+
 	while (bufz[i] != EOF)
 	{
 		while ((j = fgetc(oleFlz)) != EOF && i <= 1024)
@@ -55,36 +56,41 @@ int create_file(const char *fromFi, char *toFi)
 		}
 		writz = write(newzFlz, bufz, i);
 		if (writz == -1)
+		{
+			close(oleFlz);
+			free(bufz);
 			errz(99, toFi); /** error 99 */
+		}
 		i = 0;
 		bufz[i] = (char) j;
 	}
-	return (1);
+	return (0);
 }
 
 /**
  * errz - handles error messages
  *
- * @a: Error code
+ * @errCod: Error code
+ * @msg: Extra info to print
  */
 void errz(int errCod, char *msg)
 {
-	switch(errCod)
+	switch (errCod)
 	{
 	case 97:
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	case 98:
 		dprintf(2, "Error: Can't read from file %s\n", msg);
-		exit (98);
+		exit(98);
 	case 99:
 		dprintf(2, "Error: Can't write to %s\n", msg);
-		exit (99);
+		exit(99);
 	case 100:
 		dprintf(2, "Error assigning memory to heap");
-		exit (100);
+		exit(100);
 	default:
 		dprintf(2, "Error: Unknown error");
-		exit (101);
+		exit(101);
 	}
 }

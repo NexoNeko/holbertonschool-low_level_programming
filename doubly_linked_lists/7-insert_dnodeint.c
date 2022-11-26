be38be19e433a8fda6bc1c71540bda56df68dc41
NexoNeko <meresidamente@hotmail.com>
2022-11-26 10:40:54 -0800

    fixed neg val

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			return (add_dnodeint(h, n));
 		}
-		while (idx > 1 && temp)
+		while (idx >= 1 && temp)
 		{
 			temp = temp->next;
 			idx--;

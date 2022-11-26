54663492a4e1738bfa4000e51f687ca534167149
NexoNeko <meresidamente@hotmail.com>
2022-11-26 09:32:33 -0800

    fixed comments

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index -  returns the nth node of a listint_t linked list.
+ * get_dnodeint_at_index -  returns the nth node of a listint_t linked list.
  *
  * @head: - The linked list to go through
  * @index: - the index num desired

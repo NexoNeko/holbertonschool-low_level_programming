9dde4f3e01006c4eb9ba16c2a537ec70d4644e8c
NexoNeko <meresidamente@hotmail.com>
2022-11-26 09:43:41 -0800

    Ex 6 done

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - sums all the values contained wtihin each node in a list
+ *
+ * @head: - The linked list to go through
+ *
+ * Return: Sumof all nums
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		i += head->n;
+		head = head->next;
+	}
+	return (i);
+}
